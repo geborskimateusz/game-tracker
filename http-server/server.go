356478860d3b52c51d8dadd3eb6f5ff93c8f300f
geborskimateusz <mateuszgeborski@gmail.com>
2020-09-28 17:27:13 +0200

    working on http server

---
+++ http-server/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/player/")
+	fmt.Fprint(w, getUserScore(player))
+}
+
+func getUserScore(username string) string {
+	switch username {
+	case "Matthew":
+		return "20"
+	case "Tom":
+		return "10"
+	}
+
+	return ""
+}
---
+++ http-server/server_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	server := &PlayerServer{}
+
+	t.Run("returns score of given username", func(t *testing.T) {
+		username := "Matthew"
+
+		req := newGetScoreRequest(username)
+		res := httptest.NewRecorder()
+
+		server.ServeHTTP(res, req)
+
+		got := res.Body.String()
+		want := "20"
+
+		assertResponseBody(t, got, want)
+	})
+
+	t.Run("returns score of another username", func(t *testing.T) {
+		username := "Tom"
+
+		req := newGetScoreRequest(username)
+		res := httptest.NewRecorder()
+
+		server.ServeHTTP(res, req)
+
+		got := res.Body.String()
+		want := "10"
+
+		assertResponseBody(t, got, want)
+	})
+}
+
+func newGetScoreRequest(playername string) *http.Request {
+	url := fmt.Sprintf("/player/%s", playername)
+
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %q, got %q", got, want)
+	}
+}
